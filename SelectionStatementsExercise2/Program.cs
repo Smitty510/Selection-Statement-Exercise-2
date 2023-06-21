b5f5f5545fed1a841d04a919c521e12c25035653
patrick smith <smithpatrick.510@gmail.com>
2023-06-21 16:17:27 -0400

    exercise2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                case "calculus":
+                    Console.WriteLine("I hate math!");
+                    break;
+                case "science":
+                case "physics":
+                case "biology":
+                    Console.WriteLine("I love science!");
+                    break;
+                case "pe":
+                case "phys ed":
+                case "physical education":
+                    Console.WriteLine("PE is the best!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is fun.");
+                    break;
+                case "english":
+                    Console.WriteLine("English is boring.");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject.");
+                    break;
+
+                   
+
+
+
+
+
+
+            }
+                    
+
+
+            
+            
         }
     }
 }
